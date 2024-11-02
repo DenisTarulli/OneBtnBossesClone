fae4474d7e69a8e9aa041c2f364283e98627f23c
Denis Tarulli <132310271+DenisTarulli@users.noreply.github.com>
2024-11-02 13:35:16 -0300

    Sistema de conteo de tiempo para partida
    Cronómetro en pantalla con conteo progresivo de tiempo.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,10 +9,19 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject _winText;
     [SerializeField] private GameObject _loseText;
     [SerializeField] private GameObject _gameOverUI;
+    [SerializeField] private TextMeshProUGUI _timerText;
+    private float _time;
+    private bool _gameIsOver;
 
     private void Start()
     {
         Time.timeScale = 1f;
+    }
+
+    private void Update()
+    {
+        if (_timerText != null)
+            TimeUpdate();
     }
 
     public void StartGame()
@@ -26,6 +36,7 @@ public class GameManager : MonoBehaviour
 
     private void GameOver()
     {
+        _gameIsOver = true;
         Time.timeScale = 0f;
 
         if (FindObjectOfType<PlayerHealth>().CurrentHealth > 0)
@@ -34,6 +45,15 @@ public class GameManager : MonoBehaviour
             _loseText.SetActive(true);
 
         _gameOverUI.SetActive(true);
+    }
+    private void TimeUpdate()
+    {
+        if (_gameIsOver) return;
+
+        _time += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(_time / 60);
+        int seconds = Mathf.FloorToInt(_time % 60);
+        _timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
     private void OnEnable()
