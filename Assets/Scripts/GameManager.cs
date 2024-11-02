986b07b4cf0e828086bbbe7aff94a63923561be8
Denis Tarulli <132310271+DenisTarulli@users.noreply.github.com>
2024-11-02 14:13:41 -0300

    Cartel de victoria y derrota
    Agregado anteriormente, diferentes mensajes en el gameover dependiendo del si se ganó o se perdió el nivel.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject _winText;
     [SerializeField] private GameObject _loseText;
     [SerializeField] private GameObject _gameOverUI;
+    [SerializeField] private GameObject _finalStatsUI;
     [SerializeField] private TextMeshProUGUI _timerText;
+    [SerializeField] private TextMeshProUGUI _bestTimeText;
+    [SerializeField] private TextMeshProUGUI _finalTimeText;
     private float _time;
     private bool _gameIsOver;
 
@@ -40,12 +44,17 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0f;
 
         if (FindObjectOfType<PlayerHealth>().CurrentHealth > 0)
+        {
             _winText.SetActive(true);
+            _finalStatsUI.SetActive(true);
+            UpdateScores();
+        }
         else
             _loseText.SetActive(true);
 
         _gameOverUI.SetActive(true);
     }
+
     private void TimeUpdate()
     {
         if (_gameIsOver) return;
@@ -54,6 +63,27 @@ public class GameManager : MonoBehaviour
         int minutes = Mathf.FloorToInt(_time / 60);
         int seconds = Mathf.FloorToInt(_time % 60);
         _timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    private void UpdateScores()
+    {
+        float bestTime = PlayerPrefs.GetFloat("bestTime", 0f);
+
+        int minutes = Mathf.FloorToInt(_time / 60);
+        int seconds = Mathf.FloorToInt(_time % 60);
+
+        _finalTimeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        if (_time < bestTime || bestTime == 0f)
+        {
+            bestTime = _time;
+            PlayerPrefs.SetFloat("bestTime", bestTime);
+        }
+
+        int bestMinutes = Mathf.FloorToInt(bestTime / 60);
+        int bestSeconds = Mathf.FloorToInt(bestTime % 60);
+
+        _bestTimeText.text = string.Format("{0:00}:{1:00}", bestMinutes, bestSeconds);
     }
 
     private void OnEnable()
